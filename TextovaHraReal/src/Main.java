1cb87646863c8e0c1e549f17dca1c216fb726627
pecka <Oliverpecka210@gmail.com>
2025-03-05 11:03:50 +0100

    fixing this really broken repo #2

--- TextovaHraReal/src/Main.java
+++
@@ -1,9 +0,0 @@
-import command.Console;
-
-public class Main {
-    public static void main(String[] args) {
-
-        Console console = new Console();
-        console.start();
-    }
-}
--- TextovaHraReal/src/command/Command.java
+++
@@ -1,6 +0,0 @@
-package command;
-
-public interface Command {
-    String execute();
-    boolean exit();
-}
